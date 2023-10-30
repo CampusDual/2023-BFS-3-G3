86a6b096e06b63eb48cb88cc3e9783d46f8bcd05
Nuria <nuriaoujo.n1@gmail.com>
2023-10-30 15:28:23 +0100

    errors in gymsmaster resolved

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -4,10 +4,9 @@ import { OntimizeWebModule } from "ontimize-web-ngx";
 import { SharedModule } from "../shared/shared.module";
 import { MainRoutingModule } from "./main-routing.module";
 import { MainComponent } from "./main.component";
-import { GymsmasterHomeComponent } from './gymsmaster/gymsmaster-home/gymsmaster-home.component';
 
 @NgModule({
   imports: [SharedModule, OntimizeWebModule, MainRoutingModule],
-  declarations: [MainComponent, GymsmasterHomeComponent],
+  declarations: [MainComponent],
 })
 export class MainModule {}
