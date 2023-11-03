c145d5d0f5f07e1033e44fbd4a846d6857561c8b
Nuria <nuriaoujo.n1@gmail.com>
2023-11-04 00:26:42 +0100

    qr code error solved

--- frontend/src/app/main/access-card/access-card/access-card.component.ts
+++ frontend/src/app/main/access-card/access-card/access-card.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material';
-import { AuthService, DialogService, OFormComponent, OntimizeService, OTranslateService } from 'ontimize-web-ngx';
-
+import { AuthService, DialogService, OFormComponent, OntimizeService } from 'ontimize-web-ngx';
+ 
 @Component({
   selector: 'app-access-card',
   templateUrl: './access-card.component.html',
@@ -11,38 +10,41 @@ export class AccessCardComponent implements OnInit {
   @ViewChild('form',{static:true}) form:OFormComponent;
   public myAngularxQrCode: string = null;
   public qr: any;
-
+  public data: any = {};
   public txt:string
   constructor(
     private auth:AuthService,
     private ontimizeService: OntimizeService,
     protected dialogService: DialogService,
-    protected translateService: OTranslateService
   ) {
-    this.myAngularxQrCode = 'El usuario tiene el acceso permitido';
+   
   }
-
+ 
   ngOnInit() {
   }
   ngAfterViewInit(){
     this.form.queryData({user_:this.auth.getSessionInfo().user});
   }
-
   showQr() {
     this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('users'));
-    this.ontimizeService.query({user_:this.auth.getSessionInfo().user}, ['active','nickname'], 'user').subscribe(
+    this.ontimizeService.query({user_:this.auth.getSessionInfo().user}, ['active','name','surname'], 'user').subscribe(
       res=> {
+       
         if(res.data && res.data.length) {
-          if(res.data[0].active) {
+          this.data = res.data[0];
+          console.log('Datos recibidos:', this.data);
+         
+          if(this.data.active) {
              this.qr = document.getElementById('qrid');
              this.qr.style.display = 'block';
+             this.myAngularxQrCode = this.data.name + this.data.surname
           } else {
             this.dialogService.info('QR no generado',
             'Tienes la membresía caducada')
           }
         }
-      }
+      },
     )
-  }
-
+}
+ 
 }
