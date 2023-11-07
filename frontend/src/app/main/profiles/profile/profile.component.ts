cc0da66202d7f5976215e206813ffeef15c51ceb
marta <105271643+ffanecaa@users.noreply.github.com>
2023-11-07 10:59:55 +0100

    password validation

--- frontend/src/app/main/profiles/profile/profile.component.ts
+++ frontend/src/app/main/profiles/profile/profile.component.ts
@@ -13,8 +13,8 @@ export class ProfileComponent implements OnInit {
   @ViewChild('form',{static:true}) form:OFormComponent;
 
   public validatorArray: ValidatorFn[] = [];
-  
   public activeToggle: any;
+  public isPasswordModified: boolean = false;
 
   constructor(
     private auth:AuthService,
@@ -23,11 +23,12 @@ export class ProfileComponent implements OnInit {
     private router: Router,
     private actRoute: ActivatedRoute,
     @Inject(AuthService) private authService: AuthService,
-    ) {
+  ) {
   }
 
   ngOnInit() {
     this.validatorArray.push(this.matchValidator);
+    this.validatorArray.push(this.passwordValidator); // Agregar el validador de contraseña
 
     this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('users'));
     this.ontimizeService.query({user_:this.auth.getSessionInfo().user}, ['id_rolename'], 'deletableUser').subscribe(
@@ -38,7 +39,7 @@ export class ProfileComponent implements OnInit {
              this.activeToggle.style.display = 'block';
           }
         }
-      })
+      });
   }
 
   getValue() {
@@ -59,10 +60,10 @@ export class ProfileComponent implements OnInit {
       const passwordConfirm = control.parent ? control.parent.controls['PASSWORDCONFIRM'].value : null
       if(password && passwordConfirm && password != passwordConfirm){
         return { mustMatch: true }
-      }else{
+      } else {
         return null;
       }
-    }catch(e){
+    } catch(e){
       return null;
     }
   }
@@ -72,6 +73,23 @@ export class ProfileComponent implements OnInit {
     this.form.formGroup.get('passwordconfirm').markAsTouched();
   }
 
+  onPasswordInput() {
+    this.isPasswordModified = true;
+  }
+
+  public passwordValidator(control: any): any {
+    try {
+      const password = control.value;
+
+      if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password)) {
+        return { passwordNotValid: true };
+      } else {
+        return null;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
 
   public deleteUser(){
     this.ontimizeService.configureService(this.ontimizeService.getDefaultServiceConfiguration('users'));
@@ -80,8 +98,7 @@ export class ProfileComponent implements OnInit {
         if(res.data && res.data.length > 0) {
           if(res.data[0].id_rolename == 1 && res.data[0].activeGym >0) {
             this.dialogService.info('Tus datos no han podido ser borrados', 'Ponte en contacto con WheGym');
-          }
-          else{
+          } else {
             this.ontimizeService.delete({user_:this.auth.getSessionInfo().user}, 'user').subscribe(
               res => {
                 if(res.code == 0) {
@@ -89,9 +106,8 @@ export class ProfileComponent implements OnInit {
                   this.authService.logout();
                 }
               }
-            )
+            );
           }
-
         }
     });
   }
